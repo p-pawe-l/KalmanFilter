2394e42e2e607ba39a706b10a54f192b8ac07b98
Pawel Kozikowski <pawelkozikowski58@gmail.com>
2026-03-17 15:10:17 +0100

    Implemented det for 2x2 and 3x3 matrix

--- src/matrix.c
+++ src/matrix.c
@@ -33,6 +33,24 @@ PUBLIC matrix_t negate(const matrix_t *m) {
         return new_matrix;
 }
 
+PUBLIC matrix_t scale(const matrix_t *m, const int64_t factor) {
+	matrix_t new_matrix;
+	init_matrix(&new_matrix, m->n_rows, m->n_cols);
+	
+	const uint16_t len = m->n_rows * m->n_cols;
+	const int64_t* __restrict md = m->data;
+	
+	for ( uint16_t i = 0; i < len; ++i ) {
+		int64_t f_num  = (int64_t)
+		(
+			( (int128_t)md[i] * (int128_t)factor ) >> 32
+		);
+		new_matrix.data[i] = f_num;	
+	}
+	
+	return new_matrix;
+}
+
 
 PUBLIC matrix_t add_matrices(const matrix_t *__restrict m1, const matrix_t *__restrict m2) {
         if (m1->n_rows == m2->n_rows && m1->n_cols == m2->n_cols) {
@@ -44,8 +62,9 @@ PUBLIC matrix_t add_matrices(const matrix_t *__restrict m1, const matrix_t *__re
 		const int64_t* __restrict md1 = m1->data;
 		const int64_t* __restrict md2 = m2->data;
 
-		for ( uint16_t i = 0; i < len; ++i )
+		for ( uint16_t i = 0; i < len; ++i ) {
 			new_matrix.data[i] = md1[i] + md2[i];
+		}
 
                 return new_matrix;
         }
@@ -70,7 +89,6 @@ PUBLIC matrix_t dot_product(const matrix_t * __restrict m1, const matrix_t * __r
 					temp_res += num;	
 				}
 				temp_res = temp_res >> 32;
-				printf("Temp res: %ld\n", (int64_t)temp_res);
 				new_matrix.data[row_m1 * new_matrix.n_cols + col_m2] = (int64_t)temp_res;
 			}
 		}
@@ -83,14 +101,14 @@ PUBLIC matrix_t dot_product(const matrix_t * __restrict m1, const matrix_t * __r
 
 PUBLIC matrix_t transpose(const matrix_t* m) {
 	matrix_t new_matrix;
-	init_matrix(&new_matrix, m->n_cols, m->m_rows);
+	init_matrix(&new_matrix, m->n_cols, m->n_rows);
 	
 	const int64_t* __restrict md = m->data;
 	
 	for ( uint8_t row_m = 0; row_m < m->n_rows; ++row_m ) {
 		
 		for ( uint8_t col_m = 0; col_m < m->n_cols; ++row_m ) {
-			new_matrix.data[col_m * new_matrix.n_cols + row_m] = m->data[row_m * m->n_cols + col_m];
+			new_matrix.data[col_m * new_matrix.n_cols + row_m] = md[row_m * m->n_cols + col_m];
 		}
 	} 
 
@@ -98,48 +116,86 @@ PUBLIC matrix_t transpose(const matrix_t* m) {
 }
 
 PUBLIC int64_t det(const matrix_t* m) {
-	// Hell on earth
-
+	const int64_t* __restrict md = m->data;
+	
+	/* 2x2 Matrix */
+	if (m->n_rows == 2 && m->n_cols == 2) {
+		int64_t ad = (int64_t)
+		(
+			( (int128_t)md[0] * (int128_t)md[3] ) >> 32
+		);
+		int64_t bc = (int64_t)
+		(
+			( (int128_t)md[1] * (int128_t)md[2] ) >> 32
+		);
+		
+		return ad - bc;
+	}
+	/* 3x3 Matrix */
+	else if (m->n_rows == 3 && m->n_cols == 3) {
+		int64_t aei = (int64_t)
+		(
+			( ( ( (int128_t)md[0] * (int128_t)md[4] ) >> 32 ) * (int128_t)md[8] ) >> 32
+		);
+		int64_t bgf = (int64_t)
+		(
+			( ( ( (int128_t)md[1] * (int128_t)md[5] ) >> 32 ) * (int128_t)md[6] ) >> 32
+		);
+		int64_t cdh = (int64_t)
+		(
+			( ( ( (int128_t)md[2] * (int128_t)md[3] ) >> 32 ) * (int128_t)md[7] ) >> 32
+		);
+		
+		int64_t ceg = (int64_t)
+		(
+			( ( ( (int128_t)md[2] * (int128_t)md[4] ) >> 32 ) * (int128_t)md[6] ) >> 32
+		);
+		int64_t bdi = (int64_t)
+		(
+			( ( ( (int128_t)md[1] * (int128_t)md[3] ) >> 32 ) * (int128_t)md[8] ) >> 32
+		);
+		int64_t afh = (int64_t)
+		(
+			( ( ( (int128_t)md[0] * (int128_t)md[5] ) >> 32 ) * (int128_t)md[7] ) >> 32
+		);
+	
+		return (aei + bgf + cdh) - (ceg + bdi + afh);
+	}
+	/* other Matrix */
+	else {
+		printf("Unsupported matrix for calculating determinant!\n");
+		exit(-1);
+	}
+	
 }
 
 PUBLIC matrix_t inverse(const matrix_t* m) {
-	⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⣠⣄⢄⡤⠤⣤⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⡤⠖⠚⠉⠉⠀⠀⠄⠀⠀⡠⢀⠀⢀⡀⠉⠉⠑⠒⠢⢄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠤⠚⠉⠀⠀⢀⣠⣼⣾⣿⣶⣶⣿⣿⣿⣿⣿⣿⣿⣷⣤⡀⠐⣤⠀⠀⠙⠢⢄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡤⠋⠁⠀⢀⣠⣴⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣮⢹⣿⣷⣶⣤⡀⠙⢦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠉⠀⢀⣴⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⣀⠙⢆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠎⠀⣠⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⡀⢣⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡏⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣆⢳⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡾⢨⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡈⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡏⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣻⣯⣿⣿⣿⡿⠿⣿⢻⢞⣻⣳⣽⣮⣷⣼⣭⣭⣭⣭⣽⣯⣿⡿⠿⠿⢿⡼⠦⡀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⢯⣽⣵⡾⣷⣻⣟⡿⠿⠛⢛⣛⣛⣩⣭⣽⣷⣶⣶⣿⣿⣿⣿⣿⣿⣿⣦⡌⠢⡄⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣯⣷⡿⠿⣟⣻⣫⣭⣥⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣆⠈⣆⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⢹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣛⣿⣯⣵⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⣼⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⡎⣿⣿⣿⣿⣿⣿⣟⣿⣷⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠫⠿⢿⣿⣿⣿⣿⣿⣼⣍⣹⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠄⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢣⣽⡿⢿⣽⣿⣿⣿⣿⣿⣵⣿⣏⢻⣿⣿⣟⣿⣿⣿⣿⣿⣿⣶⣦⣿⣿⣿⡙⣿⣿⠿⣹⣿⣷⡘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⣸⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠏⣰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡷⠏⢭⣍⣤⣾⣿⣿⣿⡟⣿⣿⣿⣿⣶⡉⠄⠠⠚⣛⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣶⠁⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡟⣹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣹⡿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣹⣻⣿⣿⣿⣿⣷⣮⣕⡦⣝⢫⠟⡽⣿⣿⣿⣿⣿⣿⣿⣿⣿⢄⠇⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣿⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣯⣿⣿⣿⣿⣿⣽⢿⣿⣿⣮⣻⣵⣿⣿⣿⣿⡟⣿⡨⣿⡏⢹⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢏⡟⣹⣿⣿⣿⡀⢀⡹⣿⣿⣿⢭⣛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⠋⡇⢻⡇⢸⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢻⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣏⠾⣴⣼⣿⣿⣿⣿⣾⣿⣿⣿⣿⣶⡽⢎⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠁⠀⣧⣸⡇⣼⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣆⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣽⣻⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⢮⡽⣿⣿⣿⣿⣿⣿⡿⠃⠀⠀⢿⢼⣇⡏⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⢿⠋⣿⢯⣹⣋⣿⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣛⣟⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣩⠟⠁⠀⠀⠀⢸⡾⡿⡇⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡼⢨⣼⠛⠉⠀⠀⠀⠘⣟⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣏⣸⠁⠀⠀⠀⠀⠀⢸⣇⣧⡷⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠿⠃⠀⠀⠀⠀⠀⠀⠘⡾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢻⡃⠀⠀⠀⠀⠀⠀⠀⣿⢿⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣷⣼⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠛⠳⣄⠀⠀⠀⠀⠀⢸⣿⢸⡆⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡠⠛⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣆⡉⠑⠦⡀⠀⠀⡏⣿⣴⠇⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠋⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣿⡀⣤⡬⣷⣄⡈⠉⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡾⠁⣆⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣆⠈⠎⢷⣀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⡿⠤⠖⣿⣿⣿⢋⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣤⣥⣌⡇⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠤⠞⢁⣌⣄⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⡟⡵⢯⣷⣻⣾⣿⣿⣻⣽⣿⣿⣿⣿⣿⣿⣱⣿⢿⣿⣾⣟⠋⠙⠦⢤⣀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡤⠞⠃⢠⣴⡿⠁⣲⡿⢡⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣻⣶⣿⣿⣿⣽⣳⣿⣿⣿⣿⣿⣿⣿⣿⣷⣾⣿⣿⣿⣿⣦⡀⠳⡌⠙⠢⢀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡠⠔⠋⠀⠀⣠⣤⣾⢟⣀⣼⠿⣿⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣻⣿⣿⣿⣳⢯⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⡙⠿⣿⣿⣿⣿⣷⡀⢹⣦⣀⡀⠄
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⠔⠊⢁⣠⣴⣾⣿⣿⣿⣃⣈⣿⣄⢸⣯⣤⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⣿⠿⣿⣿⣿⡀⠹⣿⣿⡷
-⠀⠀⠀⠀⠀⠀⢀⣀⠴⠚⠁⣤⣀⣬⡿⢟⣿⣿⣿⣿⣧⣿⡿⣿⣿⣶⣹⡿⢿⣿⣿⣿⡿⣿⣿⣿⣿⣿⡿⣿⣿⣿⣿⣿⣿⣿⢟⣳⣿⣿⣿⣿⣿⣿⣿⣿⣯⣿⣿⣿⣫⣿⣿⣟⡘⢛⣿⣿⣿⣁⣽⣿⣿
-⠀⠀⠀⣀⠴⠚⠏⣤⡤⣤⡜⢻⣿⣿⣷⣮⣽⣿⣿⣿⣿⣿⣷⣿⣿⣿⡷⣿⣿⣿⣏⣿⣻⣽⢿⡿⣟⡟⠳⡈⠿⢿⣿⣿⢿⢧⣟⣻⢏⡟⣭⢡⣟⣬⣱⣿⣿⡿⣽⡟⣸⣏⣿⣿⣿⣶⣿⣿⣿⡿⣷⣿⣿
-⠀⠀⢀⡄⢲⡀⣴⣏⣿⣹⣬⢳⣿⣿⣿⣿⣿⣿⣿⣟⣻⣽⣷⣿⣿⡞⢷⡟⣯⣿⣿⣷⣻⢿⣾⢻⡼⣽⡳⣇⣎⡳⢽⣞⣯⣟⣯⣟⣯⢿⣽⣿⣿⡿⣿⣿⣿⢧⣿⠡⣿⣿⣿⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿
-⣴⣿⣿⣿⣿⣯⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣾⡿⢿⣿⣿⡌⣷⠀⣿⣿⣽⣻⢿⣻⣯⣟⣳⡽⣞⣬⢳⢿⣺⢷⣛⣮⡽⣯⣟⣾⣿⣿⢿⣿⣿⣿⣿⡟⢸⣿⣿⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣻	
+	matrix_t new_matrix;
+	
+	if ( m->n_rows == m->n_cols) {
+		int64_t m_det = det(m);
+		if (m_det == 0LL) {
+			printf("Cannot invert matrix where det=0!\n");
+			exit(-1);
+		}
+		
+		if ( m->n_rows == 2 ) {
+			init_matrix(&new_matrix, 2, 2);
+			new_matrix.data[0] = m->data[3];
+			new_matrix.data[1] = -m->data[1];
+			new_matrix.data[2] = -m->data[2];
+			new_matrix.data[3] = m->data[0];
+			
+			int64_t div_factor;
+			scale(&new_matrix, div_factor);
+		}
+	
+	}
+	else {
+		printf("Cannot invert non-square matrixes!\n");
+		exit(-1);
+	}
+	return new_matrix;
 
 }
 
