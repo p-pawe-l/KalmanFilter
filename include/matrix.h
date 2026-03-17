78b7208cbd18e7fce4e946f699dc5f7f635bbde6
Pawel Kozikowski <pawelkozikowski58@gmail.com>
2026-03-17 12:40:59 +0100

    Changed matrix and ops for other matrix data struct

--- include/matrix.h
+++ include/matrix.h
@@ -1,24 +1,62 @@
 #ifndef MATRIX_H
 #define MATRIX_H
 
+#include "other.h"
 #include <stdint.h>
 
+#define BIT_MASK_2b 0xB000000000000000
+#define BIT_MASK_4b 0xF000000000000000
+#define BIT_MASK_8b 0xFF00000000000000
+#define BIT_MASK_16b 0xFFFF000000000000
+#define BIT_MASK_32b 0xFFFFFFFF00000000
+
+typedef union {
+        int8_t __ds8_0;
+        int8_t __ds8_1;
+
+        int16_t __ds16_0;
+} union_s16_t;
+
+typedef union {
+        union_s16_t __uni_D_s16_0;
+        union_s16_t __uni_D_s16_1;
+
+        int32_t __ds32_0;
+} union_s32_t;
+
+typedef union {
+        union_s32_t __uni_D_s32_0;
+        union_s32_t __uni_D_s32_1;
+
+        int64_t __ds64_0;
+} union_s64_t;
+
+void init_union64(const int64_t __d64);
+
+union_s64_t add_uni_64b(const union_s64_t *uni1, const union_s64_t *uni2);
+
+int8_t at_uni_64b_8d(const union_s64_t *uni, const uint8_t index);
+int16_t at_uni_64b_16d(const union_s64_t *uni, const uint8_t index);
+int32_t at_uni_64b_32d(const union_s64_t *uni, const uint8_t index);
+
+void init_union32(const int32_t __d32);
+void init_union16(const int16_t __d16);
+
 typedef struct {
-        int64_t **data;
+        int64_t *data;
 
         uint8_t n_rows;
         uint8_t n_cols;
 } matrix_t;
 
-typedef int64_t *row_t;
-typedef int64_t *col_t;
-
-void init_matrix(matrix_t *m, const uint8_t n_rows, const uint8_t n_cols);
+PUBLIC void init_matrix(matrix_t *m, const uint8_t n_rows, const uint8_t n_cols);
 void destroy_matrix(matrix_t *m);
 
+int64_t at(const matrix_t *m, const uint8_t row, const uint8_t col);
+
 matrix_t negate(const matrix_t *m);
-matrix_t add_matrices(const matrix_t *m1, const matrix_t *m2);
-matrix_t dot_product(const matrix_t *m1, const matrix_t *m2);
+matrix_t add_matrices(const matrix_t * __restrict m1, const matrix_t * __restrict m2);
+matrix_t dot_product(const matrix_t * __restrict m1, const matrix_t * __restrict m2);
 matrix_t transpose(const matrix_t *m);
 matrix_t inverse(const matrix_t *m);
 
--- src/matrix.c
+++ src/matrix.c
@@ -1,82 +1,152 @@
 #include "../include/matrix.h"
 #include "../include/other.h"
+#include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 
-PUBLIC void init_matrix(matrix_t *m, const uint8_t n_rows,
-                        const uint8_t n_cols) {
+PUBLIC void init_matrix(matrix_t *m, const uint8_t n_rows, const uint8_t n_cols) {
+        m->data = malloc(sizeof(int64_t) * n_rows * n_cols);
         m->n_rows = n_rows;
         m->n_cols = n_cols;
-
-        m->data = (int64_t **)malloc(sizeof(int64_t *) * n_rows);
-        for (int i = 0; i < n_rows; i++) {
-                m->data[i] = (int64_t *)malloc(sizeof(int64_t) * n_cols);
-        }
 }
 
-PUBLIC void destroy_matrix(matrix_t *m) {
-        for (uint8_t i = 0; i < m->n_rows; i++) {
-                free(m->data[i]);
-        }
-        free(m->data);
+PUBLIC void destroy_matrix(matrix_t *m) { 
+	free(m->data); 
+}
+
+PUBLIC int64_t at(const matrix_t *m, const uint8_t row, const uint8_t col) {
+	if (row <= (m->n_rows - 1) && col <= (m->n_cols - 1))	
+		return m->data[row * m->n_cols + col];
+	exit(-1);
 }
 
 PUBLIC matrix_t negate(const matrix_t *m) {
         matrix_t new_matrix;
         init_matrix(&new_matrix, m->n_rows, m->n_cols);
-        for (uint8_t i = 0; i < m->n_rows; i++) {
-                for (uint8_t ii = 0; ii < m->n_cols; i++) {
-                        new_matrix.data[i][ii] = -m->data[i][ii];
-                }
+        
+        const uint16_t len = m->n_rows * m->n_cols;
+        const int64_t* __restrict md = m->data;
+        
+        for ( uint16_t i = 0; i < len; ++i )
+                new_matrix.data[i] = -md[i];
+        
+        return new_matrix;
+}
+
+
+PUBLIC matrix_t add_matrices(const matrix_t *__restrict m1, const matrix_t *__restrict m2) {
+        if (m1->n_rows == m2->n_rows && m1->n_cols == m2->n_cols) {
+        	const uint16_t len = m1->n_rows * m1->n_cols;
+        
+		matrix_t new_matrix;
+		init_matrix(&new_matrix, m1->n_rows, m1->n_cols);
+		
+		const int64_t* __restrict md1 = m1->data;
+		const int64_t* __restrict md2 = m2->data;
+
+		for ( uint16_t i = 0; i < len; ++i )
+			new_matrix.data[i] = md1[i] + md2[i];
+
+                return new_matrix;
         }
-
-        return new_matrix;
+        exit(-1);
 }
 
-PUBLIC matrix_t add_matrices(const matrix_t *m1, const matrix_t *m2) {
-        /* Check if same dims */
-        matrix_t new_matrix;
-        init_matrix(&new_matrix, m1->n_rows, m1->n_cols);
-        for (uint8_t i = 0; i < m1->n_rows; i++) {
-                for (uint8_t ii = 0; ii < m1->n_cols; ii++) {
-                        new_matrix.data[i][ii] =
-                            m1->data[i][ii] + m2->data[i][ii];
-                }
-        }
-
-        return new_matrix;
+PUBLIC matrix_t dot_product(const matrix_t * __restrict m1, const matrix_t * __restrict m2) {
+	if (m1->n_cols == m2->n_rows) {
+		matrix_t new_matrix;
+		init_matrix(&new_matrix, m1->n_rows, m2->n_cols);
+		
+		const int64_t* __restrict md1 = m1->data;
+		const int64_t* __restrict md2 = m2->data;
+		
+		for ( uint8_t row_m1 = 0; row_m1 < m1->n_rows; ++row_m1 ) {
+			for ( uint8_t col_m2 = 0; col_m2 < m2->n_cols; ++col_m2 ) {
+				int128_t temp_res = 0;
+				
+				for ( uint8_t col_m1 = 0; col_m1 < m1->n_cols; ++col_m1 ) {
+					int128_t num = (int128_t)md1[row_m1 * m1->n_cols + col_m1] * 
+						       (int128_t)md2[col_m1 * m2->n_cols + col_m2];	
+					temp_res += num;	
+				}
+				temp_res = temp_res >> 32;
+				printf("Temp res: %ld\n", (int64_t)temp_res);
+				new_matrix.data[row_m1 * new_matrix.n_cols + col_m2] = (int64_t)temp_res;
+			}
+		}
+		
+		return new_matrix;
+		
+	}
+	exit(-1);
 }
 
-PUBLIC matrix_t dot_product(const matrix_t *m1, const matrix_t *m2) {
-        matrix_t new_matrix;
-        init_matrix(&new_matrix, m2->n_rows, m1->n_cols);
-        for (uint8_t i = 0; i < m2->n_cols; i++) {
-                for (uint8_t ii = 0; ii < m1-> n_rows; ii++) {
-                        int128_t result = m1->data[i][ii] * m2->data[ii][i];
-                        result = result >> 32;
-                        new_matrix.data[i][ii] = (int64_t)result;
-                }
-        }
+PUBLIC matrix_t transpose(const matrix_t* m) {
+	matrix_t new_matrix;
+	init_matrix(&new_matrix, m->n_cols, m->m_rows);
+	
+	const int64_t* __restrict md = m->data;
+	
+	for ( uint8_t row_m = 0; row_m < m->n_rows; ++row_m ) {
+		
+		for ( uint8_t col_m = 0; col_m < m->n_cols; ++row_m ) {
+			new_matrix.data[col_m * new_matrix.n_cols + row_m] = m->data[row_m * m->n_cols + col_m];
+		}
+	} 
 
-        return new_matrix;
+	return new_matrix;
 }
 
-PUBLIC matrix_t transpose(const matrix_t *m) {
-        matrix_t new_matrix;
-        init_matrix(&new_matrix, m->n_cols, m->n_rows);
-        for (uint8_t i = 0; i < m->n_cols; i++) {
-                for (uint8_t ii = 0; ii < m->n_rows; i++) {
-                        new_matrix.data[i][ii] = m->data[ii][i];
-                }
-        }
+PUBLIC int64_t det(const matrix_t* m) {
+	// Hell on earth
 
-        return new_matrix;
 }
 
-PUBLIC matrix_t inverse(const matrix_t *m) {
-        matrix_t new_matrix;
-        init_matrix(&new_matrix, m->n_rows, m->n_cols);
+PUBLIC matrix_t inverse(const matrix_t* m) {
+	⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⣠⣄⢄⡤⠤⣤⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⡤⠖⠚⠉⠉⠀⠀⠄⠀⠀⡠⢀⠀⢀⡀⠉⠉⠑⠒⠢⢄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠤⠚⠉⠀⠀⢀⣠⣼⣾⣿⣶⣶⣿⣿⣿⣿⣿⣿⣿⣷⣤⡀⠐⣤⠀⠀⠙⠢⢄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡤⠋⠁⠀⢀⣠⣴⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣮⢹⣿⣷⣶⣤⡀⠙⢦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠉⠀⢀⣴⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⣀⠙⢆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠎⠀⣠⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⡀⢣⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡏⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣆⢳⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡾⢨⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡈⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡏⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣻⣯⣿⣿⣿⡿⠿⣿⢻⢞⣻⣳⣽⣮⣷⣼⣭⣭⣭⣭⣽⣯⣿⡿⠿⠿⢿⡼⠦⡀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⢯⣽⣵⡾⣷⣻⣟⡿⠿⠛⢛⣛⣛⣩⣭⣽⣷⣶⣶⣿⣿⣿⣿⣿⣿⣿⣦⡌⠢⡄⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣯⣷⡿⠿⣟⣻⣫⣭⣥⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣆⠈⣆⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⢹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣛⣿⣯⣵⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⣼⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⡎⣿⣿⣿⣿⣿⣿⣟⣿⣷⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠫⠿⢿⣿⣿⣿⣿⣿⣼⣍⣹⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠄⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢣⣽⡿⢿⣽⣿⣿⣿⣿⣿⣵⣿⣏⢻⣿⣿⣟⣿⣿⣿⣿⣿⣿⣶⣦⣿⣿⣿⡙⣿⣿⠿⣹⣿⣷⡘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⣸⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠏⣰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡷⠏⢭⣍⣤⣾⣿⣿⣿⡟⣿⣿⣿⣿⣶⡉⠄⠠⠚⣛⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣶⠁⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡟⣹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣹⡿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣹⣻⣿⣿⣿⣿⣷⣮⣕⡦⣝⢫⠟⡽⣿⣿⣿⣿⣿⣿⣿⣿⣿⢄⠇⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣿⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣯⣿⣿⣿⣿⣿⣽⢿⣿⣿⣮⣻⣵⣿⣿⣿⣿⡟⣿⡨⣿⡏⢹⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢏⡟⣹⣿⣿⣿⡀⢀⡹⣿⣿⣿⢭⣛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⠋⡇⢻⡇⢸⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢻⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣏⠾⣴⣼⣿⣿⣿⣿⣾⣿⣿⣿⣿⣶⡽⢎⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠁⠀⣧⣸⡇⣼⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣆⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣽⣻⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⢮⡽⣿⣿⣿⣿⣿⣿⡿⠃⠀⠀⢿⢼⣇⡏⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⢿⠋⣿⢯⣹⣋⣿⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣛⣟⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣩⠟⠁⠀⠀⠀⢸⡾⡿⡇⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡼⢨⣼⠛⠉⠀⠀⠀⠘⣟⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣏⣸⠁⠀⠀⠀⠀⠀⢸⣇⣧⡷⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠿⠃⠀⠀⠀⠀⠀⠀⠘⡾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢻⡃⠀⠀⠀⠀⠀⠀⠀⣿⢿⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣷⣼⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠛⠳⣄⠀⠀⠀⠀⠀⢸⣿⢸⡆⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡠⠛⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣆⡉⠑⠦⡀⠀⠀⡏⣿⣴⠇⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠋⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣿⡀⣤⡬⣷⣄⡈⠉⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡾⠁⣆⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣆⠈⠎⢷⣀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⡿⠤⠖⣿⣿⣿⢋⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣤⣥⣌⡇⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠤⠞⢁⣌⣄⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⡟⡵⢯⣷⣻⣾⣿⣿⣻⣽⣿⣿⣿⣿⣿⣿⣱⣿⢿⣿⣾⣟⠋⠙⠦⢤⣀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡤⠞⠃⢠⣴⡿⠁⣲⡿⢡⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣻⣶⣿⣿⣿⣽⣳⣿⣿⣿⣿⣿⣿⣿⣿⣷⣾⣿⣿⣿⣿⣦⡀⠳⡌⠙⠢⢀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡠⠔⠋⠀⠀⣠⣤⣾⢟⣀⣼⠿⣿⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣻⣿⣿⣿⣳⢯⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⡙⠿⣿⣿⣿⣿⣷⡀⢹⣦⣀⡀⠄
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⠔⠊⢁⣠⣴⣾⣿⣿⣿⣃⣈⣿⣄⢸⣯⣤⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⣿⠿⣿⣿⣿⡀⠹⣿⣿⡷
+⠀⠀⠀⠀⠀⠀⢀⣀⠴⠚⠁⣤⣀⣬⡿⢟⣿⣿⣿⣿⣧⣿⡿⣿⣿⣶⣹⡿⢿⣿⣿⣿⡿⣿⣿⣿⣿⣿⡿⣿⣿⣿⣿⣿⣿⣿⢟⣳⣿⣿⣿⣿⣿⣿⣿⣿⣯⣿⣿⣿⣫⣿⣿⣟⡘⢛⣿⣿⣿⣁⣽⣿⣿
+⠀⠀⠀⣀⠴⠚⠏⣤⡤⣤⡜⢻⣿⣿⣷⣮⣽⣿⣿⣿⣿⣿⣷⣿⣿⣿⡷⣿⣿⣿⣏⣿⣻⣽⢿⡿⣟⡟⠳⡈⠿⢿⣿⣿⢿⢧⣟⣻⢏⡟⣭⢡⣟⣬⣱⣿⣿⡿⣽⡟⣸⣏⣿⣿⣿⣶⣿⣿⣿⡿⣷⣿⣿
+⠀⠀⢀⡄⢲⡀⣴⣏⣿⣹⣬⢳⣿⣿⣿⣿⣿⣿⣿⣟⣻⣽⣷⣿⣿⡞⢷⡟⣯⣿⣿⣷⣻⢿⣾⢻⡼⣽⡳⣇⣎⡳⢽⣞⣯⣟⣯⣟⣯⢿⣽⣿⣿⡿⣿⣿⣿⢧⣿⠡⣿⣿⣿⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿
+⣴⣿⣿⣿⣿⣯⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣾⡿⢿⣿⣿⡌⣷⠀⣿⣿⣽⣻⢿⣻⣯⣟⣳⡽⣞⣬⢳⢿⣺⢷⣛⣮⡽⣯⣟⣾⣿⣿⢿⣿⣿⣿⣿⡟⢸⣿⣿⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣻	
 
-        // TODO
-
-        return new_matrix;
 }
+
+
+
+
+
+
+
+
