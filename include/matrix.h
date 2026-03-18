c87bfd3d91422b06da71de27a798490cec2e8432
Pawel Kozikowski <pawelkozikowski58@gmail.com>
2026-03-18 08:00:02 +0100

    Added part of the documentation for matrix.h

--- include/matrix.h
+++ include/matrix.h
@@ -2,66 +2,107 @@
 #define MATRIX_H
 
 #include "other.h"
+#include "fixed_point/fp_arithmetic.h"
+
 #include <stdint.h>
 
-#define BIT_MASK_2b 0xB000000000000000
-#define BIT_MASK_4b 0xF000000000000000
-#define BIT_MASK_8b 0xFF00000000000000
-#define BIT_MASK_16b 0xFFFF000000000000
-#define BIT_MASK_32b 0xFFFFFFFF00000000
-
-typedef union {
-        int8_t __ds8_0;
-        int8_t __ds8_1;
-
-        int16_t __ds16_0;
-} union_s16_t;
-
-typedef union {
-        union_s16_t __uni_D_s16_0;
-        union_s16_t __uni_D_s16_1;
-
-        int32_t __ds32_0;
-} union_s32_t;
-
-typedef union {
-        union_s32_t __uni_D_s32_0;
-        union_s32_t __uni_D_s32_1;
-
-        int64_t __ds64_0;
-} union_s64_t;
-
-void init_union64(const int64_t __d64);
-
-union_s64_t add_uni_64b(const union_s64_t *uni1, const union_s64_t *uni2);
-
-int8_t at_uni_64b_8d(const union_s64_t *uni, const uint8_t index);
-int16_t at_uni_64b_16d(const union_s64_t *uni, const uint8_t index);
-int32_t at_uni_64b_32d(const union_s64_t *uni, const uint8_t index);
-
-void init_union32(const int32_t __d32);
-void init_union16(const int16_t __d16);
-
-typedef struct {
-        int64_t *data;
+/*
+ * Representation of linear matrix
+ * What does it mean, linear ? Example:
+ * Normal matrix would look like this and would be normally stored as two 
+ * dimensional array (FP_TYPE **). But this could be inefficient way of 
+ * storing numbers like this
+ * |a b|
+ * |c d|
+ *
+ * Linear matrix data is stored as single array. Thanks to that are data is
+ * placed in contigous way. This potenital enables vector operations on our
+ * CPU with a little optimization needed.
+ * |a b|c d|
+ * 
+ * attribute data (FP_TYPE*) - array with numbers
+ * n_rows         (uint8_t ) - number of rows of our matrix
+ * n_cols         (uint8_t ) - number of columns in our matrix
+ * 
+ * We still need to track number of columns and rows to check whether
+ * matrices can be added or multiplied etc.
+ * */
+struct linear_matrix {
+        FP_TYPE *data;
 
         uint8_t n_rows;
         uint8_t n_cols;
-} matrix_t;
+};
 
-#define HAS_SAME_DIMS(m1, m2) ( ( m1->n_rows == m2->n_rows ) && ( m1->n_cols == m2->n_cols ) )
-#define CAN_MULTUIPLY(m1, m2) ( m1->n_cols == m2->n_rows ) 
+#define HAS_SAME_DIMS(m1, m2) 	( ( m1->n_rows == m2->n_rows ) && ( m1->n_cols == m2->n_cols ) )
+#define IS_SQUARE_MATRIX(m1)	( m1->n_rows == m1->n_cols )
+#define CAN_MULTUIPLY(m1, m2) 	( m1->n_cols == m2->n_rows ) 
 
-PUBLIC void init_matrix(matrix_t *m, const uint8_t n_rows, const uint8_t n_cols);
-void destroy_matrix(matrix_t *m);
+/*
+ * Initializing matrix function
+ * Initialize matrix data attribute with sufficient memory (malloc used), assign rows and cols
+ * to proper attributes
+ * 
+ * param m 	(struct linear_matrix *) - pointer to the matrix that will be initialized
+ * param n_rows (const uint8_t         ) - number of rows that initialized matrix will have
+ * param n_cols (const uint8_t	       ) - number of columns that initialized matrix will have
+ * 
+ * return void
+ * */
+PUBLIC void init_matrix(struct linear_matrix *m, const uint8_t n_rows, const uint8_t n_cols);
 
-int64_t at(const matrix_t *m, const uint8_t row, const uint8_t col);
+/*
+ * Destroying matrix`s resources function
+ * Just release memory of array that each matrix is holding (free)
+ * 
+ * param m	(struct linear_matrix *) - pointer to matrix that resources will be desotryed
+ *
+ * return void
+ * */
+PUBLIC void destroy_matrix(struct linear_matrix *m);
 
-matrix_t negate(const matrix_t *m);
-matrix_t add_matrices(const matrix_t * __restrict m1, const matrix_t * __restrict m2);
-matrix_t dot_product(const matrix_t * __restrict m1, const matrix_t * __restrict m2);
-matrix_t transpose(const matrix_t *m);
-int64_t det(const matrix_t* m);
-matrix_t inverse(const matrix_t *m);
+/*
+ * Fetching number from provided index function
+ * User provides number of row and column that he is willing to get a number from
+ * and this function just calculates position and fetch the number
+ *
+ * param m	(const struct linear_matrix *) - pointer to the matrix that we will be fetching from
+ * param row	(const uint8_t 		     ) - number of the row to fetch from
+ * param col    (const uint8_t		     ) - number of the column to fetch from 
+ *
+ * return	(FP_TYPE		     ) - fetched number in 64-bit/32-bit fixed-point format
+ * */
+PUBLIC FP_TYPE at(const struct linear_matrix *m, const uint8_t row, const uint8_t col);
+
+
+/*
+ * Negating all of the numbers in the matrix function 
+ * Takes all of our numbers in matrix and negate them
+ *
+ * param	(const struct linear_matrix *) - pointer to the matrix that will be negate
+ *
+ * return	(struct linear_matrix        ) - new matrix with negated values
+ * */
+PUBLIC struct linear_matrix negate(const struct linear_matrix *m);
+
+/*
+ * Adding two matrices function
+ * Take all of the numbers from matrix-1 array and matrix-2 array, add them together on
+ * matching indexes and BOOM, addition performed.
+ *
+ * param m1	(const struct linear_matrix * __restrict) - pointer to the first matrix  (__restrict for vectorized operations on CPU)
+ * parma m2	(const struct_linear_matrix * __restrict) - pointer to the second matrix (__restrict for vectorized operations on CPU) 
+ * 
+ * return 	(struct linear_matrix			) - new matrix that results from adding two matrices to each other
+ * */
+PUBLIC struct linear_matrix add_matrices(const matrix_t * __restrict m1, const matrix_t * __restrict m2);
+
+PUBLIC struct linear_matrix dot_product(const matrix_t * __restrict m1, const matrix_t * __restrict m2);
+
+PUBLIC struct linear_matrix transpose(const matrix_t *m);
+
+PUBLIC FP_TYPE det(const matrix_t* m);
+
+PUBLIC struct linear_matrix inverse(const matrix_t *m);
 
 #endif
