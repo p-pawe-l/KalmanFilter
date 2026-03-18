3f77d80831969f9974d588ba2f909f25472dd384
Pablo <pawelkozikowski58@gmail.com>
2026-03-18 11:58:29 +0100

    Refactored matrix.c

--- include/other.h
+++ include/other.h
@@ -1,12 +1,10 @@
 #ifndef OTHER_H
 #define OTHER_H
 
-typedef __int128 int128_t;
-typedef unsigned __int128 uint128_t;
-
 #define PUBLIC
-#define PRIVATE static
-#define EXTERN extern
+#define PRIVATE         static
+#define EXTERN          extern
+#define INLINE          inline
 
 #define OPERATION_FAILURE -1
 
--- src/matrix.c
+++ src/matrix.c
@@ -6,6 +6,27 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define LM_SUCCESS(lm)          ( LINEAR_MATRIX_RESULT ) { .result = lm, .status_code = SUCCESS }
+#define LM_ERROR(lm, ec)        ( LINEAR_MATRIX_RESULT ) { .result = lm, .status_code = ec }
+#define FP_SUCCESS(fp)          ( FP_TYPE_RESULT ) { .result = fp, .status_code = SUCCESS }
+#define FP_ERROR(ec)            ( FP_TYPE_RESULT ) { .result = 0, .status_code = ec }
+
+#define LM_VALIDATE_ERR_C(m, ec)                                                                                \
+        if ( ec != SUCCESS )                                                                                    \
+                return ( LINEAR_MATRIX_RESULT ) { .result =  m, .status_code = ec}                              \
+                
+#define FP_VALIDATE_ERR_C(ec)                                                                                   \
+        if ( ec != SUCCESS )                                                                                    \
+                return ( FP_TYPE_RESULT ) { .result = 0, .status_code = ec}                                     \
+
+#define VALIDATE_M_DATA_FP_TYPE(m)                                                                              \
+                if ( !m->data )                                                                                 \
+                        return ( FP_TYPE_RESULT ) { .result = 0, .status_code = ERR_NULL_DATA_POINTER }         \
+
+#define VALIDATE_M_DATA_LM_TYPE(m, nm)                                                                          \
+                if ( !m->data )                                                                                 \
+                        return ( LINEAR_MATRIX_RESULT ) { .result = nm, .status_code = ERR_NULL_DATA_POINTER }  \
+
 PUBLIC mo_status_t init_matrix(linear_matrix_t *m, const uint32_t n_rows, const uint32_t n_cols) 
 {
 	if (n_rows == 0 || n_cols == 0)
@@ -23,7 +44,7 @@ PUBLIC mo_status_t init_matrix(linear_matrix_t *m, const uint32_t n_rows, const 
 	
 }
 
-PUBLIC mo_status_t destroy_matrix_LINEAR(linear_matrix_t *m) 
+PUBLIC mo_status_t destroy_matrix(linear_matrix_t *m) 
 { 
 	if (m->data)
 	{
@@ -39,129 +60,140 @@ PUBLIC mo_status_t destroy_matrix_LINEAR(linear_matrix_t *m)
 
 PUBLIC FP_TYPE_RESULT at(const linear_matrix_t *m, const uint32_t row, const uint32_t col) 
 {
+        VALIDATE_M_DATA_FP_TYPE(m);
 	if ( row < m->n_rows && col < m->n_cols )
 	{	
-		return (FP_TYPE_RESULT) { .result = m->data[row * m->n_cols + col], .status_code = SUCCESS };
+		return FP_SUCCESS(m->data[row * m->n_cols + col]);
 	}
 	else
-	{
-		return (FP_TYPE_RESULT) { .result = 0, .status_code = ERR_INDEX_OUT_OF_RANGE };
-	}
+        {
+                return FP_ERROR(ERR_INDEX_OUT_OF_RANGE);
+        }
+	
 }
 
 PUBLIC LINEAR_MATRIX_RESULT negate(const linear_matrix_t *m) 
 {
-        struct linear_matrix new_Lmatrix;
-        init_matrix(&new_Lmatrix, m->n_rows, m->n_cols);
-        
-        const uint16_t len = m->n_rows * m->n_cols;
+        linear_matrix_t new_matrix;
+	mo_status_t result = init_matrix(&new_matrix, m->n_rows, m->n_cols);
+        LM_VALIDATE_ERR_C(new_matrix, result);
+
+        const uint64_t len = m->n_rows * m->n_cols;
+
+        VALIDATE_M_DATA_LM_TYPE(m, new_matrix);
         const FP_TYPE* __restrict md = m->data;
         
-        for ( uint16_t i = 0; i < len; ++i )
+        for ( uint64_t i = 0; i < len; ++i )
 	{
-                new_Lmatrix.data[i] = -md[i];
+                new_matrix.data[i] = -md[i];
 	}        
 
-        return new_matrix;
+        return LM_SUCCESS(new_matrix);
 }
 
-
-PUBLIC struct linear_matrix scale(const struct linear_matrix *m, const int64_t factor) 
+PUBLIC LINEAR_MATRIX_RESULT scale(const linear_matrix_t *m, const int64_t factor) 
 {
-	struct linear_matrix new_Lmatrix;
-	init_matrix_LINEAR(&new_Lmatrix, m->n_rows, m->n_cols);
-	
-	const uint16_t len = m->n_rows * m->n_cols;
+	struct linear_matrix new_matrix;
+	mo_status_t result = init_matrix(&new_matrix, m->n_rows, m->n_cols);
+	LM_VALIDATE_ERR_C(new_matrix, result);
+
+	const uint64_t len = m->n_rows * m->n_cols;
+        VALIDATE_M_DATA_LM_TYPE(m, new_matrix);
 	const FP_TYPE* __restrict md = m->data;
 	
-	for ( uint16_t i = 0; i < len; ++i ) 
+	for ( uint64_t i = 0; i < len; ++i ) 
 	{
-		new_Lmatrix.data[i] = fp_multiply(md[i], factor);	
+		new_matrix.data[i] = fp_multiply(md[i], factor);	
 	}
 	
-	return new_matrix;
+	return LM_SUCCESS(new_matrix);
 }
 
-
-PUBLIC struct linear_matrix add_matrices(const struct linear_matrix *__restrict m1, const struct linear_matrix *__restrict m2) 
+PUBLIC LINEAR_MATRIX_RESULT add_matrices(const linear_matrix_t *__restrict m1, const linear_matrix_t *__restrict m2) 
 {
+	linear_matrix_t new_matrix;
         if HAS_SAME_DIMS(m1, m2) 
 	{
-        	const uint16_t len = m1->n_rows * m1->n_cols;
-        
-		struct linear_matrix new_Lmatrix;
-		init_matrix_LINEAR(&new_matrix, m1->n_rows, m1->n_cols);
-		
+		mo_status_t result = init_matrix(&new_matrix, m1->n_rows, m1->n_cols);
+		LM_VALIDATE_ERR_C(new_matrix, result);
+
+        	const uint64_t len = m1->n_rows * m1->n_cols;
+
+                VALIDATE_M_DATA_LM_TYPE(m1, new_matrix);
 		const FP_TYPE* __restrict md1 = m1->data;
+
+                VALIDATE_M_DATA_LM_TYPE(m2, new_matrix);
 		const FP_TYPE* __restrict md2 = m2->data;
 		
-		for ( uint16_t i = 0; i < len; ++i )
+		for ( uint64_t i = 0; i < len; ++i )
 		{
-			new_Lmatrix.data[i] = fp_add(md1[i], md2[i]);
+			new_matrix.data[i] = fp_add(md1[i], md2[i]);
 		}
 
-                return new_Lmatrix;
+                return LM_SUCCESS(new_matrix);
         } 
-	else
+        else
         {
-		log_error("add_matrices", "Matrices must to have the same dimensions!");
-		exit(OPERATION_FAILURE);
+		return LM_ERROR(new_matrix, ERR_DIMENSION_MISMATCH);
 	}
 }
 
-PUBLIC struct linear_matrix dot_product(const struct linear_matrix *__restrict m1, const struct linear_matrix *__restrict m2) 
+PUBLIC LINEAR_MATRIX_RESULT dot_product(const linear_matrix_t *__restrict m1, const linear_matrix_t *__restrict m2) 
 {
+	linear_matrix_t new_matrix;
 	if CAN_MULTIPLY(m1, m2) 
 	{
-		struct linear_matrix new_Lmatrix;
-		init_matrix(&new_Lmatrix, m1->n_rows, m2->n_cols);
-		
+		mo_status_t result = init_matrix(&new_matrix, m1->n_rows, m2->n_cols);
+		LM_VALIDATE_ERR_C(new_matrix, result);
+
+                VALIDATE_M_DATA_LM_TYPE(m1, new_matrix);
 		const FP_TYPE* __restrict md1 = m1->data;
+
+                VALIDATE_M_DATA_LM_TYPE(m2, new_matrix);
 		const FP_TYPE* __restrict md2 = m2->data;
 		
-		for ( uint8_t row_m1 = 0; row_m1 < m1->n_rows; ++row_m1 ) 
+		for ( uint32_t row_m1 = 0; row_m1 < m1->n_rows; ++row_m1 ) 
 		{
-			for ( uint8_t col_m2 = 0; col_m2 < m2->n_cols; ++col_m2 ) 
+			for ( uint32_t col_m2 = 0; col_m2 < m2->n_cols; ++col_m2 ) 
 			{
 				FP_TYPE temp_res = 0;
-				for ( uint8_t col_m1 = 0; col_m1 < m1->n_cols; ++col_m1 ) 
+				for ( uint32_t col_m1 = 0; col_m1 < m1->n_cols; ++col_m1 ) 
 				{
 					FP_TYPE num1 = md1[row_m1 * m1->n_cols + col_m1]; 
 					FP_TYPE num2 = md2[col_m1 * m2->n_cols + col_m2];	
 					temp_res += fp_multiply(num1, num2);	
 				}
-				new_Lmatrix.data[row_m1 * new_Lmatrix.n_cols + col_m2] = temp_res;
+				new_matrix.data[row_m1 * new_matrix.n_cols + col_m2] = temp_res;
 			}
 		}
 		
-		return new_matrix;
-		
+		return LM_SUCCESS(new_matrix);
 	}
 	else 
 	{
-		log_error("dot_product", "Matrix-1 need to have the same number of columns, as Matrix-2 rows!");
-		exit(OPERATION_FAILED);
+		return LM_ERROR(new_matrix, ERR_DIMENSION_MISMATCH);
 	}
 }
 
-PUBLIC struct linear_matrix transpose(const struct linear_matrix *m) 
+PUBLIC LINEAR_MATRIX_RESULT transpose(const linear_matrix_t *m) 
 {
-	struct linear_matrix new_Lmatrix;
-	init_matrix(&new_Lmatrix, m->n_cols, m->n_rows);
-	
+	linear_matrix_t new_matrix;
+	mo_status_t result = init_matrix(&new_matrix, m->n_cols, m->n_rows);
+	LM_VALIDATE_ERR_C(new_matrix, result);
+
+        VALIDATE_M_DATA_LM_TYPE(m, new_matrix);
 	const FP_TYPE* __restrict md = m->data;
 	
-	for ( uint8_t row_m = 0; row_m < m->n_rows; ++row_m ) 
+	for ( uint32_t row_m = 0; row_m < m->n_rows; ++row_m ) 
 	{	
-		for ( uint8_t col_m = 0; col_m < m->n_cols; ++col_m ) 
+		for ( uint32_t col_m = 0; col_m < m->n_cols; ++col_m ) 
 		{
-			new_Lmatrix.data[col_m * new_matrix.n_cols + row_m] = md[row_m * m->n_cols + col_m];
+			new_matrix.data[col_m * new_matrix.n_cols + row_m] = md[row_m * m->n_cols + col_m];
 		}
 	} 
 
-	return new_matrix;
+	return LM_SUCCESS(new_matrix);
 }
-
 
 /*
  * Helper function for calculating part of the determinant of the 3x3 matrix.
@@ -184,15 +216,16 @@ PRIVATE FP_TYPE for_det_calc_help(const FP_TYPE x1, const FP_TYPE x2, const FP_T
 	return fp_multiply( fp_multiply( x1, x2 ), x3 );
 }
 
-PUBLIC FP_TYPE det(const struct linear_matrix *m) {
-	const FP_TYPE* __restrict md = m->data;
+PUBLIC FP_TYPE_RESULT det(const linear_matrix_t *m) {
+        VALIDATE_M_DATA_FP_TYPE(m);
+        const FP_TYPE* __restrict md = m->data;
 	
 	if (m->n_rows == 2 && m->n_cols == 2) 
 	{
 		FP_TYPE ad = fp_multiply(md[0], md[3]);
 		FP_TYPE bc = fp_multiply(md[1], md[2]);
 		
-		return fp_substract(ad, db);
+		return FP_SUCCESS(fp_substract(ad, bc));
 	}
 	else if (m->n_rows == 3 && m->n_cols == 3) 
 	{
@@ -204,12 +237,11 @@ PUBLIC FP_TYPE det(const struct linear_matrix *m) {
 		FP_TYPE bdi = for_det_calc_help(md[1], md[3], md[8]);	
 		FP_TYPE afh = for_det_calc_help(md[0], md[5], md[7]);	
 		
-		return (aei + bgf + cdh) - (ceg + bdi + afh);
+		return FP_SUCCESS((aei + bgf + cdh) - (ceg + bdi + afh));
 	}
 	else 
 	{
-		log_error("det", "Unsupported matrix provided! - Only 2x2 and 3x3 ;//");
-		exit(OPERATION_FAILURE);
+		return FP_ERROR(ERR_UNSUPPORTED_TYPE);
 	}
 	
 }
@@ -219,50 +251,47 @@ PRIVATE FP_TYPE cofactor_calc_help(const FP_TYPE x1, const FP_TYPE x2, const FP_
 	return fp_substract( fp_multiply( x1, x2 ) , fp_multiply( x3, x4 ) );
 }
 
-PUBLIC matrix_t inverse(const struct linear_matrix *m) {
-	struct linear_matrix new_Lmatrix;
+PUBLIC LINEAR_MATRIX_RESULT inverse(const linear_matrix_t *m) {
+	linear_matrix_t new_matrix;
 	
+        VALIDATE_M_DATA_LM_TYPE(m, new_matrix);
 	const FP_TYPE* __restrict md = m->data;
-	FP_TYPE determinant = det(m);	
 
-	if ( IS_SQUARE_MATRIX(m) && determimant != AS_FP(0LL) ) 
+	FP_TYPE_RESULT determinant_res = det(m);	
+        LM_VALIDATE_ERR_C(new_matrix, determinant_res.status_code);
+
+	if ( IS_SQUARE_MATRIX(m) && determinant_res.result != AS_FP(0LL) ) 
 	{
-		FP_TYPE div_factor = fp_divide(AS_FP(1LL), determinant);
+		FP_TYPE div_factor = fp_divide(AS_FP(1LL), determinant_res.result);
 		if ( m->n_rows == 2 ) 
 		{
-			init_matrix(&new_Lmatrix, 2, 2);
-			
+			mo_status_t result = init_matrix(&new_matrix, 2, 2);
+			LM_VALIDATE_ERR_C(new_matrix, result);
+
 			new_matrix.data[0] = md[3];
 			new_matrix.data[1] = -md[1];
 			new_matrix.data[2] = -md[2];
 			new_matrix.data[3] = md[0];
 			
-			new_Lmatrix = scale(&new_Lmatrix, div_factor);
+			LINEAR_MATRIX_RESULT scaling_result = scale(&new_matrix, div_factor);
+                        LM_VALIDATE_ERR_C(new_matrix, scaling_result.status_code);
+                        new_matrix = scaling_result.result;
 		}
 		else if (m->n_rows == 3) 
 		{
-			init_matrix(&new_Lmatrix, 3, 3);
+			init_matrix(&new_matrix, 3, 3);
 			
 			FP_TYPE c00 = cofactor_calc_help(md[4], md[8], md[5], md[7]);
 			FP_TYPE c10 = -cofactor_calc_help(md[1], md[8], md[2], md[7]);
 			FP_TYPE c20 = cofactor_calc_help(md[1], md[5], md[2], md[4]); 			
-		
+                        // TODO
 		}
 	
 	}
 	else 
 	{
-		log_error("inverse", "Cannot inverse non-square matrixes or with determinant=0");
-		exit(OPERATION_FAILURE);
+                return LM_ERROR(new_matrix, ERR_SINGULAR_MATRIX || ERR_DIMENSION_MISMATCH);
 	}
-	return new_matrix;
+	return LM_SUCCESS(new_matrix);
 
 }
-
-
-
-
-
-
-
-
