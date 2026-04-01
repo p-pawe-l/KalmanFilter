c81b928bbb46fbc40c6c2e9b68311566d760a191
Pablo <pawelkozikowski58@gmail.com>
2026-04-01 23:13:14 +0200

    vis scripts

--- scripts/vis.py
+++ scripts/vis.py
@@ -1,10 +1,33 @@
+import typing
 import dataclasses
 
 
-@dataclasses.dataclass
-class Object_1DIM:
-    pos: float 
-    vel: float
-    acc: float
+one_dim_state = tuple[float, float, float]
+
+# Can call proc there and in C put output to stdout and read it here 
+# to process it as a plot for example 
+
+
+@typing.final
+class ProbingProgram:
+    def __init__(self, program_path: str) -> None:
+        self.program_path: str = program_path
+
+    def get_probe(self) -> typing.Sequence[float]:
+        pass
+
+
+@typing.final
+class MultiDimObject:
+    def __init__(self, probing_program: str, steps: int) -> None:
+        self.states: typing.Sequence[one_dim_state] = states
+        self.steps: int = steps
+
+        self.proc = subprocess.Popen()
+
+    def run_sim(self) -> None:
+        pass
+
+        
     
     
